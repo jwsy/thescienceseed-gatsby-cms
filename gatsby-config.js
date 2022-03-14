4c4e5e2c929f9538db6e5664a495ac09bcfa191a
Jason Yee <446031+jwsy@users.noreply.github.com>
2022-03-13 20:29:47 -0400

    Change gatsby-plugin-netlify-identity to -demo.ml website

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-identity`,
       options: {
-        url: `https://thescienceseed-gatsby-cms.netlify.app` // required!
+        url: `https://thescienceseed-demo.ml` // required!
       }
     },
     {
